bed4cb529184d2739bfb335eeada02724080c305
Safei Ashraf <44810632+Safei-Ashraf@users.noreply.github.com>
2020-02-14 13:18:01 +0200

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,47 +1,47 @@
 // Select color input
 // Select size input
-let color, height, width;
-
-const formInput = document.getElementById('sizePicker');
-
-formInput.addEventListener('submit', function (event){
-  event.preventDefault();
-  height= document.getElementById('inputHeight').value;
-  width= document.getElementById('inputWidth').value;
- // TEST Input Is Recorded Correctly To Variables:
- // console.log(`Height is ${height} and width is ${width}`);
 
 // When size is submitted by the user, call makeGrid()
-  makeGrid(height,width);
+let height, width;
+
+const formInput = document.querySelector('#sizePicker');
+
+formInput.addEventListener('submit',function(e)
+{    //PREVENT Default Behaviour on SUBMIT FORM.
+    e.preventDefault();
+    //STORE height and width Values from FORM INPUT.
+    height = document.querySelector('#inputHeight').value;
+    width = document.querySelector('#inputWidth').value;
+    //CALLING makeGrid function to build Grid once  Input Added.
+    makeGrid(height,width);
 });
 
-color = document.getElementById('colorPicker').value;
-//RESET Grid On New Selection
-function resetGrid(table){
- table.innerHTML="";
-}
- // NAKE Grid Upon Selecting Height and Width
-function makeGrid(h,w) 
+//RESET Grid on New Submission;
+function resetGrid(table)
 {
-  //REMOVE All Table Cells
-  let table = document.getElementById('pixelCanvas');
-  resetGrid(table);
-  for(let i = 1; i <= h ;i++)
-  {
-    let tableRow = document.createElement('tr');
-    for(let j = 1; j <= w; j++)
-    {
-      let cellId = i+'-'+j;
-      let tableData = document.createElement('td');
-      tableData.setAttribute('id',cellId);
-      let colorPicker = document.getElementById('colorPicker');
+    table.innerHTML = '';
+};
 
-      tableData.addEventListener('click', function()
-      {
-        this.style.backgroundColor =  document.getElementById('colorPicker').value;
-      });
-      tableRow.appendChild(tableData);
+function makeGrid(h,w)
+ {
+    // Your code goes here!
+    let table = document.querySelector('#pixelCanvas');
+    resetGrid(table);
+    for(let i = 0; i <=h; ++i)
+    {
+        let tableRow = document.createElement('tr');
+        for(let j = 1; j <= w; ++j) 
+        {
+            let tableData = document.createElement('td');
+       
+        //ONCLICK Change Color:
+        tableData.addEventListener('click',function()
+        {
+            this.style.backgroundColor  = document.querySelector('#colorPicker').value;
+        });
+
+             tableRow.appendChild(tableData);
+         }
+        table.appendChild(tableRow);
     }
-    table.appendChild(tableRow);
-  }
 };
